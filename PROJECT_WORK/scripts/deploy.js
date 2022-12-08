59616dc30e48faa6e12db9255649e592a34ef202
Samar4eg <81931426+Samar4eg@users.noreply.github.com>
2022-12-08 17:50:20 +0000

    Update deploy.js

--- PROJECT_WORK/scripts/deploy.js
+++ PROJECT_WORK/scripts/deploy.js
@@ -1,7 +1,17 @@
 async function deploy() {
     const HelperStaff = await ethers.getContractFactory("HelperStaff")
     const helperStaff = await HelperStaff.deploy()
+    const WAIT_BLOCK_CONFIRMATIONS = 6;
+    await helperStaff.deployed()
     console.log("Contract deployed to address:", helperStaff.address)
+     await helperStaff.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS);
+    console.log(`Deployed contract to: ${helperStaff.address}`)
+    console.log(`Verifying contract on Etherscan...`);
+
+  await hre.run(`verify:verify`, {
+    address: helperStaff.address,
+    constructorArguments: [],
+  });
 }
 
 deploy()
